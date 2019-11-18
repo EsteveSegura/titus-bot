ad83ffced341bef3293e0344df8a0344fdcbac8b
Esteve Segura <estevesegura.gir@gmail.com>
2019-11-18 22:22:17 +0100

    events

--- src/bot.js
+++ src/bot.js
@@ -27,12 +27,51 @@ client.on("chat", (channel, user, message, self) => {
 
      //console.log(user)
      let rawBadges = Object.values(user)
-     if(rawBadges[rawBadges.length-3] != null){
-          if(rawBadges[rawBadges.length-3].includes("founder")){
-               console.log("SI1")
-          }else{
-               console.log("NO")
+     if (rawBadges[rawBadges.length - 3] != null) {
+          if (rawBadges[rawBadges.length - 3].includes("founder")) {
+               //console.log("SI1")
+          } else {
+               //console.log("NO")
           }
      }
-     socket.emit('voiceChat', { message: message, isMod: user.mod || false, isSuscriber: user.subscriber || false, author: user.username  });
+     socket.emit('voiceChat', { message: message, isMod: user.mod || false, isSuscriber: user.subscriber || false, author: user.username });
+});
+
+client.on("resub", (channel, username, months, message, userstate, methods) => {
+     //let meses = ~~userstate["msg-param-cumulative-months"];
+     //let mess = userstate["msg-param-should-share-streak"];
+     console.log(`channel: ${channel} - username: ${username} - months: ${months} - message: ${message} - userstate: ${userstate}`)
+     //socket.emit('resub', {});
+});
+
+client.on("subgift", (channel, username, streakMonths, recipient, methods, userstate) => {
+     //let senderCount = ~~userstate["msg-param-sender-count"];
+     console.log(`channel: ${channel} - username: ${username} - streakMonths: ${streakMonths} - recipient: ${recipient} - userstate: ${userstate}`)
+     //socket.emit('subgift', {});
+});
+
+client.on("submysterygift", (channel, username, numbOfSubs, methods, userstate) => {
+     //let senderCount = ~~userstate["msg-param-sender-count"];
+     console.log(`channel: ${channel} - username: ${username} - numbOfSubs: ${numbOfSubs} - userstate: ${userstate}`)
+     //socket.emit('submysterygift', {});
+});
+
+client.on("subscription", (channel, username, method, message, userstate) => {
+     console.log(`channel: ${channel} - username: ${username} - message: ${message} - userstate: ${userstate}`)
+     //socket.emit('subscription', {});
+});
+
+client.on("cheer", (channel, userstate, message) => {
+     console.log(`channel: ${channel} - message: ${message} - userstate: ${userstate}`)
+     //socket.emit('cheer', {});
+});
+
+client.on("hosted", (channel, username, viewers, autohost) => {
+     console.log(`channel: ${channel} - username: ${username} - viewers: ${viewers}`)
+     //socket.emit('hosted', {});
+});
+
+client.on("vips", (channel, vips) => {
+     console.log(`channel: ${channel} - vips: ${vips}`)
+     //socket.emit('vips', {});
 });
